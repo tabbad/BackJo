38442531e41afca38c8148d29911676baa121c4e
Your Name <theo.abbadie@gmail.com>
2023-12-30 22:13:53 +0100

    teste de base

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 //read account.json file
-const fs = require('fs');
-const data = fs.readFileSync('account.json');
-const accounts = JSON.parse(data);
+//const fs = require('fs');
+//const data = fs.readFileSync('account.json');
+//const accounts = JSON.parse(data);
 
 
 app.get('/home', (req, res) => {
    //return data 
-    res.send(accounts)
+    res.send("accounts")
 
 })
 
