9e28851442ea7c02cdbef1369042231c2bea27a9
Your Name <theo.abbadie@gmail.com>
2023-12-30 20:21:40 +0100

    test

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+  const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
+  
+  module.exports = allowCors(handler)
 
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
@@ -12,6 +35,9 @@ app.get('/home', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+
+
 
 // Export the Express API
 module.exports = app
