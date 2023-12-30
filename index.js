e20536ae83b57b8c3601d14d4ae5be73720ad2bc
Your Name <theo.abbadie@gmail.com>
2023-12-30 21:47:05 +0100

    Test2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 4000
 //read account.json file
 const fs = require('fs');
-const data = await fs.readFile(process.cwd() + 'account.json', 'utf8');
+const data = fs.readFileSync('./account.json');
 const accounts = JSON.parse(data);
 
 
