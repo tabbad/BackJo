d4b6b49596b963b7986953ee3ce2d8b0d2b27448
Your Name <theo.abbadie@gmail.com>
2023-12-31 18:33:00 +0100

    rajout post

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // index.js
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
 const PORT = 4000
 //read account.json file
@@ -13,13 +15,12 @@ function readAccount() {
 //const data = fs.readFileSync('account.json');
 //const accounts = JSON.parse(data);
 
+app.use(bodyParser.json());
 
 app.get('/home', (req, res) => {
    //return data 
     //res.send(accounts)
     res.status(200).json('Welcome, your app is working well');
-   
-
 })
 
 app.get('/recuperer-json', (req, res) => {
@@ -41,6 +42,33 @@ app.get('/recuperer-json', (req, res) => {
   });
 });
 
+app.post('/modifier-json', (req, res) => {
+  const filePath = path.join(__dirname, 'nom-du-fichier.json');
+
+  // Lire le fichier JSON depuis le système de fichiers
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Erreur lors de la lecture du fichier JSON :', err);
+      res.status(500).json({ error: 'Erreur lors de la lecture du fichier JSON' });
+      return;
+    }
+
+    // Modifier les données en fonction du corps de la requête POST
+    const modifiedData = req.body; // Supposons que le corps de la requête POST contient les modifications nécessaires
+    const newData = { ...JSON.parse(data), ...modifiedData };
+
+    // Écrire les modifications dans le fichier JSON
+    fs.writeFile(filePath, JSON.stringify(newData, null, 2), 'utf8', (err) => {
+      if (err) {
+        console.error('Erreur lors de l\'écriture du fichier JSON :', err);
+        res.status(500).json({ error: 'Erreur lors de l\'écriture du fichier JSON' });
+        return;
+      }
+
+      res.json({ message: 'Fichier JSON modifié avec succès' });
+    });
+  });
+});
 
 
 app.listen(PORT, () => {
